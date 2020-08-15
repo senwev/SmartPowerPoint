bc9bff3294b84486763d6f23940a569e3271026d
王梓宇 <wangzy_ncepu@hust.edu.cn>
2020-08-15 22:25:42 +0800

    Home App

--- MouseClick/Drawing/Viewer3D.Designer.cs
+++ MouseClick/Drawing/Viewer3D.Designer.cs
@@ -38,21 +38,25 @@ namespace MouseClick
             this.viewer3DControl1.AllowDrop = true;
             this.viewer3DControl1.BackColor = System.Drawing.SystemColors.Window;
             this.viewer3DControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.viewer3DControl1.Location = new System.Drawing.Point(0, 0);
+            this.viewer3DControl1.Location = new System.Drawing.Point(10, 10);
             this.viewer3DControl1.MinimumSize = new System.Drawing.Size(10, 10);
             this.viewer3DControl1.Name = "viewer3DControl1";
-            this.viewer3DControl1.Size = new System.Drawing.Size(825, 506);
+            this.viewer3DControl1.Size = new System.Drawing.Size(805, 486);
             this.viewer3DControl1.TabIndex = 2;
             // 
             // Viewer3D
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(825, 506);
             this.Controls.Add(this.viewer3DControl1);
             this.Name = "Viewer3D";
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "Viewer3D";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Viewer3D_FormClosing);
+            this.Load += new System.EventHandler(this.Viewer3D_Load);
             this.ResumeLayout(false);
 
         }
